c817bd01e78a589fc92434f7b5505e64ffe20ff4
Jasmine Unochi <jasmineunochii@gmail.com>
2024-06-25 17:33:11 +0700

    latest
    bug di bagian favorite tab:
    movie detail harus pindah ke home tab.
    layout-ing masih ancur.
    
    screen movie detail juga belum selesai.

--- src/navigations/HomeStackNavigation.tsx
+++ src/navigations/HomeStackNavigation.tsx
@@ -1,16 +1,17 @@
-// src/navigations/HomeStackNavigator.tsx
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Home from '../screens/Home'
-import MovieDetail from '../screens/MovieDetail'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import MovieDetail from '../screens/MovieDetail';
+import Favorite from '../screens/Favorite';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
-export default function HomeStackNavigator(): JSX.Element {
+export default function HomeStackNavigation(): JSX.Element {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Favorite" component={Favorite} />
       <Stack.Screen name="MovieDetail" component={MovieDetail} />
     </Stack.Navigator>
-  )
+  );
 }
